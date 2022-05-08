e480069fc13ee70e1ab3f1ec43c0762ec4addf23
sh <lop9036@naver.com>
2022-05-08 17:06:33 +0900

    #4 시큐리티(로그인,로그아웃,회원가입)

---
+++ src/test/java/com/shop/controller/ItemControllerTest.java
@@ -0,0 +1,41 @@
+package com.shop.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations="classpath:application-test.properties")
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("상품 등록 페이지 권한 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN") //회원이름,role이 admin인 유저가 로그인된 상태로 테스트를 할 수 있도록
+    public void itemFormTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new")) //상품등록페이지에 get요청
+                .andDo(print()) //요청,응답 메세지 확인하도록 콘솔창 출력
+                .andExpect(status().isOk()); //응답상태코드 정상인지 확인
+    }
+
+    @Test
+    @DisplayName("상품 등록 페이지 일반 회원 접근 테스트")
+    @WithMockUser(username = "user", roles = "USER")
+    public void itemFormNotAdminTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new"))
+                .andDo(print())
+                .andExpect(status().isForbidden()); //Forbidden 예외가 발생하면 테스트 성공적 통과
+    }
+}
